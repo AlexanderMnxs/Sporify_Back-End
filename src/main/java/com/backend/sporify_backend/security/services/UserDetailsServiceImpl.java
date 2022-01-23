8a97646c3717bfbe04db600099740227a82a5c65
AlexanderMnxs <86621469+AlexanderMnxs@users.noreply.github.com>
2022-01-23 19:03:10 +0200

    Reuploading Back-End through new Repository

---
+++ src/main/java/com/backend/sporify_backend/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,33 @@
+package com.backend.sporify_backend.security.services;
+
+import com.example.springboot.models.User;
+import com.example.springboot.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+/**
+ * Είναι απαραίτητης για το security της spring boot
+ * Έχει μια μέθοδο που βρίσκει τον χρήστη απο το username
+ * που περνάμε ως όρισμα. Αν δεν τον βρεί πετάει UsernameNotFoundException.
+ */
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	UserRepository userRepository;
+
+	@Override
+	@Transactional
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findByUsername(username)
+				.orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+		return UserDetailsImpl.build(user);
+	}
+
+}
---
+++ src/test/java/com/backend/sporify_backend/SporifyBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.sporify_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SporifyBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
