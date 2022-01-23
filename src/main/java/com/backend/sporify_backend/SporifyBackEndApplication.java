178aaf94cda6160ee4bf86331e85d8bc2d0cdd75
AlexanderMnxs <86621469+AlexanderMnxs@users.noreply.github.com>
2022-01-24 00:51:36 +0200

    Reuploading LoginResponse and MessageResponse + Adding commandLineRunner on SporifyBackEndApplication for testing a random user

--- src/main/java/com/backend/sporify_backend/SporifyBackEndApplication.java
+++ src/main/java/com/backend/sporify_backend/SporifyBackEndApplication.java
@@ -1,7 +1,12 @@
 package com.backend.sporify_backend;
 
+import com.backend.sporify_backend.models.User;
+import com.backend.sporify_backend.repositories.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class SporifyBackEndApplication {
@@ -10,4 +15,24 @@ public class SporifyBackEndApplication {
         SpringApplication.run(SporifyBackEndApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner commandLineRunner(UserRepository userRepository, PasswordEncoder encoder) {
+        return (args) -> {
+
+            // create new user's account
+            User user = new User("test",
+                    "test@test.com",
+                    encoder.encode("12345678"));
+
+            // if user doesn't already exist
+            // create user
+            if(!userRepository.existsByUsername(user.getUsername())){
+                userRepository.insert(user);
+
+                return;
+            }
+
+            System.out.println("Test user already exists!");
+        };
+    }
 }
